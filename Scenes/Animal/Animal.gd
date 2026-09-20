904a98a02f786d41e3d3102472ed83d766de522a
Chrysovalantis Siomos <SIOPIS2008@HOTMAIL.COM>
2026-09-20 21:12:55 +0300

    lock space (box) for dragging animal and sound draging with click

--- Scenes/Animal/Animal.gd
+++ Scenes/Animal/Animal.gd
@@ -2,18 +2,62 @@ class_name Animal
 
 extends RigidBody2D
 
-@onready var label: Label = $Label
+const DRAG_LIM_MAX: Vector2 = Vector2(0, 60)
+const DRAG_LIM_MIN: Vector2 = Vector2(-60, 0)
 
+
+@onready var label: Label = $Label
+@onready var arrow: Sprite2D = $Arrow
+@onready var stretch_sound: AudioStreamPlayer2D = $StretchSound
+
+var _start: Vector2 = Vector2.ZERO
+var _drag_start: Vector2 = Vector2.ZERO
+var _dragged_vector: Vector2 = Vector2.ZERO
+var _is_dragging: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_start = position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	label.text = "Freeze:%s\nCOntactCount:%d\nSleeping:%s" % [
+	var debug_str: String = "FR:%s CC:%d SL:%s\n" % [
 		freeze,
 		get_contact_count(),
 		sleeping
 	]
+	debug_str += "is_dragging:%s drag_start:%.0f, %.0f\n" % [
+		_is_dragging, _drag_start.x, _drag_start.y
+	]
+	debug_str += "_dragged_vector:%.0f, %.0f" % [
+		_dragged_vector.x, _dragged_vector.y
+	]
+	label.text = debug_str
+
+func _physics_process(delta: float) -> void:
+	if _is_dragging: handle_dragging()
+
+
+func handle_dragging() -> void:
+	var new_dragged_vector: Vector2 = get_global_mouse_position() - _drag_start
+	new_dragged_vector = new_dragged_vector.clamp(DRAG_LIM_MIN, DRAG_LIM_MAX)
+	
+	var diff: Vector2 = new_dragged_vector - _dragged_vector
+	if diff.length() > 0 and !stretch_sound.playing:
+		stretch_sound.play()
+	
+	_dragged_vector = new_dragged_vector
+	position = _start + _dragged_vector
+
+
+func start_dragging() -> void:
+	arrow.show()
+	_is_dragging = true
+	_drag_start = get_global_mouse_position()
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed("drag"):
+		input_event.disconnect(_on_input_event)
+		start_dragging()
