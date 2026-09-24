590f830e613c2fa1a6d48d12b1af1f9816cb7bce
Chrysovalantis Siomos <SIOPIS2008@HOTMAIL.COM>
2026-09-24 21:00:39 +0300

    add water in main scene - worldBoundaryShade, sound, sprite, die func

--- Scenes/Animal/Animal.gd
+++ Scenes/Animal/Animal.gd
@@ -84,6 +84,9 @@ func scale_arrow() -> void:
 	arrow.scale.x = lerpf(_arrow_scale_x, _arrow_scale_x * 2, perc)
 	arrow.rotation = (_start - position).angle()
 
+func die() -> void:
+	queue_free()
+
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("drag"):
